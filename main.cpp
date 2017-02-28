324edcd3852cd15c9d4b9e0584133acd99fddb21
Alexey Khotchenkov <hotchenkov@list.ru>
2017-02-28 14:56:21 +0300

    сброка socket.io

--- main.cpp
+++ main.cpp
@@ -22,3 +22,16 @@ int main(int argc, char *argv[])
 
     return app.exec();
 }
+
+
+/*
+
+установка socket.io-cpp
+git clone --recurse-submodules https://github.com/socketio/socket.io-client-cpp.git
+cd socket.io-client-cpp
+
+cmake -DBOOST_ROOT=../..//home/alexey/boost_1_63_0/stage/lib -DBOOST_VER=1.63
+
+make install
+
+*/
